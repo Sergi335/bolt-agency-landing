edb06d64e02b81c961f214d3f4f0c711a16422fa
Sergi335 <sergiadn335@gmail.com>
2025-10-19 23:49:07 +0200

    refactor: actualizar dependencias de Tailwind CSS y agregar soporte para tipografía

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -90,5 +91,6 @@ export default {
         }
       }
     }
-  }
+  },
+  plugins: [typography]
 }
